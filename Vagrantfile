08c6076d4147a9e5ffb4bb57160566f95624f03f
ldes-cou <ldes-cou@student.42.fr>
2023-01-09 16:29:07 +0100

    creating server and serverworker VMS with docker installed

--- Vagrantfile
+++ Vagrantfile
@@ -1,19 +1,48 @@
 Vagrant.configure("2") do |config|
-   config.vm.box = "ubuntu/trusty64"
-   # config.vm.url = "file://sgoinfre/goinfre/Perso/ldes-cou/ldes-couS.box"
-   config.vm.synced_folder '.', '/IOT'
-   config.vm.provider "virtualbox" do |vb|
-      # vb.customize ["modifyvm", :id, "--name","ldes-couS"]
-      vb.name = "ldes-couS"
-      vb.memory = 1024
-      # vb.check_guest_additions = false
-      vb.cpus = 1
-      vb.gui = true
-      # override.vm.box_download_checksum_type
-      # override.vm.box_download_checksum 
-      # override.vm.box_url
+   servers=[
+      {
+         :hostname => "ldes-couS",
+         :ip => "192.168.56.110",
+         # :ssh_port => '2200'
+
+      },
+      {
+         :hostname => "ldes-couSW",
+         :ip => "192.168.56.111",
+         # :ssh_port => '2400'
+      }
+   ]
+
+   servers.each do |machine|
+      config.vm.define machine[:hostname] do |node|
+         node.vm.box = "bento/ubuntu-16.04"
+         node.vm.hostname = machine[:hostname]
+         node.vm.network :private_network, ip: machine[:ip]
+         # node.vm.network "forwarded_port", guest: 22, host: machine[:ssh_port], id: "ssh"
+         node.vm.provision :shell, path: "bootstrap.sh"
+         node.vm.synced_folder '.', '/IOT'
+         node.vm.provider "virtualbox" do |vb|
+            vb.customize ["modifyvm", :id, "--memory", 1024]
+            vb.customize ["modifyvm", :id, "--cpus", 1]
+            vb.name = machine[:hostname]
+            vb.gui = true
+         end
+      end
    end
-   config.vm.provision :shell, path: "bootstrap.sh"
-      # control.vm.provision "shell", path: "control.sh"
- end
+end
+
+#    # config.vm.box = "ubuntu/trusty64"
+#    config.vm.synced_folder '.', '/IOT'
+#    config.vm.hostname = $HOSTNAME
+
+#    config.vm.network "private_network", ip: "192.168.57.3"
+#    config.vm.provider "virtualbox" do |vb|
+#       vb.customize ["modifyvm", :id, "--memory", 1024]
+#       vb.customize ["modifyvm", :id, "--cpus", 1]
+#       vb.name = "ldes-couS"
+#       vb.gui = true
+#    end
+#    config.vm.provision :shell, path: "bootstrap.sh"
+#       # control.vm.provision "shell", path: "control.sh"
+#  end
  
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,52 +2,41 @@
 
 echo "coucou ici le script"
 
-#!/bin/bash
 # abort this script on errors.
-set -eux
+# set -eux
 
-# prevent apt-get et al from opening stdin.
-# NB even with this, you'll still get some warnings that you can ignore:
-#     dpkg-preconfigure: unable to re-open stdin: No such file or directory
-export DEBIAN_FRONTEND=noninteractive
+echo "install docker"
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+sudo apt update
+apt-cache search docker-ce
+sudo apt install containerd.io docker-ce docker-ce-cli docker-ce-rootless-extras 
 
 
+echo "add your user to docker group"
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+# newgrp docker
 
-echo "install the desktop."
-apt-get update
+# echo "installing k3s"
+# echo "setup the master k3s node" 
+# curl -sfL https://get.k3s.io | sh -s - --docker
 
-apt-get install -y --no-install-recommends \
-    xorg \
-    xserver-xorg-video-qxl \
-    xserver-xorg-video-fbdev \
-    xserver-xorg-video-vmware \
-    xfce4 \
-    xfce4-terminal \
-    lightdm \
-    lightdm-gtk-greeter \
-    xfce4-whiskermenu-plugin \
-    xfce4-taskmanager \
-    menulibre 
-    # firefox
+# echo "allow port on firewall"
+# sudo ufw allow 6443/tcp
+# sudo ufw allow 443/tcp
 
-# # install useful tools.
-# apt-get install -y --no-install-recommends git-core meld
-# apt-get install -y --no-install-recommends httpie
-# apt-get install -y --no-install-recommends vim
-echo "install guest additions"
-apt-get install build-essential module-assistant
-apt-get install -y --no-install-recommends virtualbox-guest-x11
-m-a prepare
+# echo "install k3s on workers node and connect them to master"
+# curl -sfL http://get.k3s.io | K3S_URL=https://<master_IP>:6443 K3S_TOKEN=<join_token> sh -s - --docker
+
 
 echo "don't forget to install  ks3"
 
-apt-get remove -y --purge xscreensaver
-apt-get autoremove -y --purge
 echo "->Installation finished! login with vagrant:vagrant"
 
-if [ ! -f ~/.first_shutdown ]; then
-	touch ~/.first_shutdown
+if [ ! -f ~/.first_boot ]; then
+	touch ~/.first_boot
 	echo "->Shutting down to finish install, re-run vagrant up"
-	poweroff
+	reboot
 fi
-
