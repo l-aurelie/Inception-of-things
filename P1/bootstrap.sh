4a03e5026bb8be298d93de22835bb3784681e50f
Aurelie LECONTE <aleconte@e3r5p20.clusters.42paris.fr>
2023-01-11 17:51:58 +0100

    replace Ubuntu by Alpine317

--- P1/bootstrap.sh
+++ P1/bootstrap.sh
@@ -3,38 +3,26 @@ HOSTNAME=$(hostname)
 IP_MASTER=192.168.56.110
 IP_AGENT=192.168.56.111
 
-echo "coucou ici le script"
+echo "=== Installing docker"
+sudo apk add docker
+sudo addgroup vagrant docker
+sudo rc-update add docker boot
 
-#apt-get update
-
-#echo "install docker"
-#sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-#sudo apt update 
-#apt-cache search docker-ce
-#sudo apt install -y docker-ce
-
-
-#echo "add your user to docker group"
-#sudo groupadd docker
-#sudo usermod -aG docker vagrant
-
-echo "installing k3s"
-echo "setup the master k3s node" 
+echo "=== Installing k3s"
 if [ $(hostname) = "ldes-couS" ]; then
-    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $IP_MASTER --bind-address=$IP_MASTER --advertise-address=$IP_MASTER" sh - # -s - --docker
+    echo "Setup the master k3s node"
+    curl -sfL https://get.k3s.io | sed "s/sourcex/source/g" | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $IP_MASTER --bind-address=$IP_MASTER --advertise-address=$IP_MASTER" sh - # -s - --docker
+    sleep 10
     sudo cat /var/lib/rancher/k3s/server/node-token > /IOT/node-token
-#    echo $TOKEN
 fi
 
-echo "joining master - agent nodes "
 if [ $(hostname) = "ldes-couSW" ]; then
-    curl -sfL http://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$IP_MASTER:6443 --node-ip=$IP_AGENT" K3S_TOKEN=$(cat /IOT/node-token) sh - # -s - --docker
+    echo "Joining master - agent nodes "
+    curl -sfL http://get.k3s.io | sed "s/sourcex/source/g" | INSTALL_K3S_EXEC="agent --server https://$IP_MASTER:6443 --node-ip=$IP_AGENT" K3S_TOKEN=$(cat /IOT/node-token) sh - # -s - --docker
 fi
 
 
-echo "->Installation finished! login with vagrant:vagrant"
+echo "->Installation finished!"
 
 if [ ! -f ~/.first_boot ]; then
 	touch ~/.first_boot
