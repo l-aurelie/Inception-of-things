dd3d982487b14ee6d13ba83e51e1679c6292c93b
Aurelie LECONTE <aleconte@e3r5p20.clusters.42paris.fr>
2023-01-11 17:05:01 +0100

    functionnal with ip addresses

--- P1/bootstrap.sh
+++ P1/bootstrap.sh
@@ -1,60 +1,36 @@
 #!/bin/bash
 HOSTNAME=$(hostname)
 IP_MASTER=192.168.56.110
+IP_AGENT=192.168.56.111
 
 echo "coucou ici le script"
 
-# abort this script on errors.
-# set -eux
-# export DEBIAN_FRONTEND=noninteractive
+#apt-get update
 
-echo "install the desktop."
-apt-get update
-
-# apt-get install -y --no-install-recommends \
-#     xorg \
-#     xserver-xorg-video-qxl \
-#     xserver-xorg-video-fbdev \
-#     xserver-xorg-video-vmware \
-#     xfce4 \
-#     xfce4-terminal \
-#     lightdm \
-#     lightdm-gtk-greeter \
-#     xfce4-whiskermenu-plugin \
-#     xfce4-taskmanager \
-#     menulibre 
-
-echo "install docker"
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
-sudo apt update 
-apt-cache search docker-ce
-sudo apt install -y docker-ce
+#echo "install docker"
+#sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu xenial stable"
+#sudo apt update 
+#apt-cache search docker-ce
+#sudo apt install -y docker-ce
 
 
-echo "add your user to docker group"
-sudo groupadd docker
-sudo usermod -aG docker vagrant
+#echo "add your user to docker group"
+#sudo groupadd docker
+#sudo usermod -aG docker vagrant
 
 echo "installing k3s"
-
 echo "setup the master k3s node" 
-if [ $HOSTNAME = "ldes-couS" ]; then
-    echo "CACACACAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-    curl -sfL https://get.k3s.io | sh -s - --docker
+if [ $(hostname) = "ldes-couS" ]; then
+    curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $IP_MASTER --bind-address=$IP_MASTER --advertise-address=$IP_MASTER" sh - # -s - --docker
     sudo cat /var/lib/rancher/k3s/server/node-token > /IOT/node-token
-    echo $TOKEN
-    echo "allow port on firewall"
-    sudo ufw allow 6443/tcp
-    sudo ufw allow 443/tcp
+#    echo $TOKEN
 fi
 
-
 echo "joining master - agent nodes "
-if [ $HOSTNAME = "ldes-couSW" ]; then
-     echo "COUIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIIII"
-    curl -sfL http://get.k3s.io | K3S_URL=https://$IP_MASTER:6443 K3S_TOKEN=$(cat /IOT/node-token) sh -s - --docker
+if [ $(hostname) = "ldes-couSW" ]; then
+    curl -sfL http://get.k3s.io | INSTALL_K3S_EXEC="agent --server https://$IP_MASTER:6443 --node-ip=$IP_AGENT" K3S_TOKEN=$(cat /IOT/node-token) sh - # -s - --docker
 fi
 
 
