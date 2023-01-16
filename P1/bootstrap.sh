6cf9431a4bf6aaabf17bb3c69291e75af292e66d
Aurelie LECONTE <aleconte@e2r1p4.clusters.42paris.fr>
2023-01-16 17:45:17 +0100

    waiting for token, reload instead of reboot to keep shared folder working

--- P1/bootstrap.sh
+++ P1/bootstrap.sh
@@ -12,7 +12,7 @@ echo "=== Installing k3s"
 if [ $(hostname) = "ldes-couS" ]; then
     echo "Setup the master k3s node"
     curl -sfL https://get.k3s.io | sed "s/sourcex/source/g" | INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip $IP_MASTER --bind-address=$IP_MASTER --advertise-address=$IP_MASTER" sh - # -s - --docker
-    sleep 10
+    while [ ! -f /var/lib/rancher/k3s/server/token ]; do sleep 1; done
     sudo cat /var/lib/rancher/k3s/server/node-token > /IOT/node-token
 fi
 
@@ -24,8 +24,8 @@ fi
 
 echo "->Installation finished!"
 
-if [ ! -f ~/.first_boot ]; then
-	touch ~/.first_boot
-	echo "->Shutting down to finish install"
-	sudo reboot
-fi
+#if [ ! -f ~/.first_boot ]; then
+#	touch ~/.first_boot
+#	echo "->Shutting down to finish install"
+#	sudo reboot
+#fi
