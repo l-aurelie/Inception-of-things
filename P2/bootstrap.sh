8680ab907e5cc2ccbf6a68c9070d764e16b0f8c8
ldes-cou <ldes-cou@student.42.fr>
2023-01-16 18:38:42 +0100

    app1 is working on port 80

--- P2/bootstrap.sh
+++ P2/bootstrap.sh
@@ -12,7 +12,7 @@ echo "Setup the master k3s node"
 MYSECRET=iambatman
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644"
 curl -fL https://get.k3s.io  | sed "s/sourcex/source/g" | K3S_TOKEN=${MYSECRET} \
-    sh -s - --disable traefik server 
+    sh -s - --docker #--disable traefik server 
 
 echo "->Installation finished!"
 
