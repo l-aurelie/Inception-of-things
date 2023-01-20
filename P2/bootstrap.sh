2cd1c5183d36fa014b9b2b4518bc54461462ed53
ldes-cou <ldes-cou@student.42.fr>
2023-01-20 16:17:40 +0100

    changing hostname && cleaning a bit

--- P2/bootstrap.sh
+++ P2/bootstrap.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 
 echo "=== Installing k3s"
-echo "Setup the k3s node"
+echo "=== Setting up k3s' node"
 
-MYSECRET=iambatman
+
 export INSTALL_K3S_EXEC="--write-kubeconfig-mode=644 --node-ip=192.168.56.110"
-curl -fL https://get.k3s.io  | sed "s/sourcex/source/g" | K3S_TOKEN=${MYSECRET} \
-    sh -s - 
-while [ ! -e /var/lib/rancher/k3s/server/token ]
-do
-    sleep 1
-done
+curl -fL https://get.k3s.io  | sed "s/sourcex/source/g" | sh -s - #| K3S_TOKEN=${MYSECRET} \
+
 sleep 15
 
-echo "Deploy pods, services and ingress "
+echo "=== Deploying pods, services and ingress"
 kubectl apply -f /IOT/pods/app1-deployment.yaml
 kubectl apply -f /IOT/pods/app2-deployment.yaml
 kubectl apply -f /IOT/pods/app3-deployment.yaml
