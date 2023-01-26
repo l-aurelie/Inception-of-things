35ffdf1d6188a4786e2d34cf68f6b274fb8fee85
ldes-cou <ldes-cou@student.42.fr>
2023-01-26 12:33:21 +0100

    argocd setup complete

--- P3/launcher.sh
+++ P3/launcher.sh
@@ -2,7 +2,6 @@ sudo apt-get update
 sudo apt-get install -y vim
 sudo apt-get remove docker docker-engine docker.io containerd runc
 
-
 echo "###Installing Docker"
 echo"->Setup docker repo"
 sudo apt-get install -y \
@@ -40,31 +39,29 @@ curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/b
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 
-echo "->Create k3d cluster"
-k3d cluster create argocd --api-port 127.0.0.1:6445 --port '8888:80@loadbalancer'
-
-echo "###Installing argcd"
-kubectl create namespace argocd
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.0-rc5/manifests/ha/install.yaml
-
 echo "##Download argocd CLI"
 sudo curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 sudo rm argocd-linux-amd64
 
-echo "##Log in to argocd server"
-export ARGOCD_OPTS='--port-forward-namespace argocd'
-argocd login 172.18.0.3 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
-argocd account update-password
+echo "->Create k3d cluster"
+k3d cluster create argocd --api-port 127.0.0.1:6445 --port '8888:80@loadbalancer'
 
-# kubectl config set-context --current --namespace=argocd
+echo "==> Creating namespace argocd"
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.0-rc5/manifests/ha/install.yaml
 
-# echo "##To connect to the service without exposing it / connect with localhost:8080"
-# #kubectl port-forward svc/argocd-server -n argocd 8080:443
+sleep 20
 
 echo "===> Deploy will's app on dev namespace"
 kubectl create namespace dev
-kubectl apply -n dev -f /media/sf_P3/deployment.yaml
+kubectl apply -n argocd -f /media/sf_P3/argocd.yaml
 kubectl apply -n dev -f /media/sf_P3/ingress.yaml
 
+echo "##To connect to the service without exposing it / connect with localhost:8080"
+export ARGOCD_OPTS='--port-forward-namespace argocd'
+kubectl port-forward svc/argocd-server -n argocd 8080:443
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+# argocd login localhost:8080 --insecure --username admin --password $(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo)
+# argocd account update-password
 
