632c982f45c8642d7ec6ff1b3b4b6597281ea1aa
ldes-cou <ldes-cou@student.42.fr>
2023-01-27 14:03:29 +0100

    fixing get_helm script typo

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -14,10 +14,11 @@ sudo curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/release
 sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
 sudo rm argocd-linux-amd64
 
+echo "### Installing git"
+sudo apk add git
+
 echo "### Install helm "
-sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-sudo chmod 700 get_helm.sh
-./get_helm.sh
+bash /IOT/scripts/get_helm.sh
 
 # echo "### Install GitLab"
 # echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/main" >> /etc/apk/repositories
