0bdf38828388eb1c8c5553364f571b706849d69e
Aurelie LECONTE <aleconte@e3r5p14.clusters.42paris.fr>
2023-01-30 14:57:13 +0100

    install from minimum-value.yaml

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -27,9 +27,10 @@ echo "Install gitlab "
 sudo kubectl create namespace gitlab
 sudo helm repo add gitlab https://charts.gitlab.io/
 sudo helm repo update
-sudo helm upgrade --install -n gitlab gitlab gitlab/gitlab --set global.hosts.domain=192.168.56.110.nip.io --set global.hosts.externalIP=192.168.56.110 --set certmanager-issuer.email=email@example.com
-
-#-f https://gitlab.com/gitlab-org/charts/gitlab/-/blob/master/examples/values-minikube-minimum.yaml
+sudo helm upgrade --install gitlab gitlab/gitlab \
+  -n gitlab \
+  -f https://gitlab.com/gitlab-org/charts/gitlab/raw/master/examples/values-minikube-minimum.yaml \
+  --set global.hosts.domain=192.168.56.110.nip.io --set global.hosts.externalIP=192.168.56.110 --set certmanager-issuer.email=email@example.com 2>/dev/null
 
 # echo "==> Creating namespaces
 # sudo kubectl create namespace argocd
