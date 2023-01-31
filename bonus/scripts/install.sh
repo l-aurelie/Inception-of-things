c9bacd0c2882367b601ab16b61c8eb196c0321de
ldes-cou <ldes-cou@student.42.fr>
2023-01-31 11:09:21 +0100

    merging

--- bonus/scripts/install.sh
+++ bonus/scripts/install.sh
@@ -53,22 +53,17 @@ sudo kubectl port-forward --address 0.0.0.0 svc/gitlab-webservice-default -n git
 # sudo kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.0-rc5/manifests/ha/install.yaml
 # sudo kubectl apply -n argocd -f /IOT/confs/argocd.yaml
 # sudo kubectl apply -n dev -f /IOT/confs/ingress.yaml
-# while [[ $(kubectl get pods -n dev -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; \
-#     do echo "[DEV] Waiting for pods to be ready..." && sleep 10;
-# done
+while [[ $(kubectl get pods -n dev -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; \
+    do echo "[DEV] Waiting for pods to be ready..." && sleep 10;
+done
 
+echo "Adding "
 
-# echo "##Forwarding port to access service from outside : connect with 192.168.56.110:8080"
-# export ARGOCD_OPTS='--port-forward-namespace argocd'
+echo "##Forwarding port to access service from outside : connect with 192.168.56.110:8080"
+export ARGOCD_OPTS='--port-forward-namespace argocd'
 
-# echo -en "\033[36m      PASSWORD=="; sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
-# echo -en "\033[0m"
-# sudo kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443 
-
-
-# echo "### Install GitLab"
-# echo "http://dl-cdn.alpinelinux.org/alpine/v3.12/main" >> /etc/apk/repositories
-# apk update
-# apk add gitlab-ce
-# gitlab-ctl reconfigure
+echo -en "\033[36m      ARGOCD_PASSWORD: "; sudo kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d; echo
+echo -en "\033[36m      GITLAB_PASSWORD: "; sudo kubectl get secret gitlab-gitlab-initial-root-password -n gitlab -ojsonpath={.data.password} | base64 -d ; echo
+echo -en "\033[0m"
+sudo kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443 | sudo kubectl port-forward --address 0.0.0.0 gitlab-webservice-default-5c96966546-prltr -n gitlab 8585:8181
 
